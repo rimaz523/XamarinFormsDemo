14111372ab6a8dbdb0ee747d9781dc102f21ae64
Rimaz <rimazmohommed523@gmail.com>
2020-02-27 10:37:07 +1300

    add new screens

---
+++ XamarinDemo/XamarinDemo/Common/Colors.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinDemo.Common
+{
+    public static class Colors
+    {
+        public static Color QTPurple = Color.FromHex("2a2559");
+        public static Color QTRed = Color.FromHex("e80a4d");
+        public static Color QTOrange = Color.FromHex("fc671a");
+    }
+}
