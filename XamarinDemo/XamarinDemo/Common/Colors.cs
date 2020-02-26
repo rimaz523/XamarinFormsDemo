82c19ed082859028ec23458ebaeb6ce40584b484
Rimaz <rimazmohommed523@gmail.com>
2020-02-27 11:48:10 +1300

    use color palette in your pages

--- XamarinDemo/XamarinDemo/Common/Colors.cs
+++ XamarinDemo/XamarinDemo/Common/Colors.cs
@@ -4,8 +4,10 @@ namespace XamarinDemo.Common
 {
     public static class Colors
     {
+        public static Color QTWhite = Color.FromHex("ffffff");
         public static Color QTPurple = Color.FromHex("2a2559");
         public static Color QTRed = Color.FromHex("e80a4d");
         public static Color QTOrange = Color.FromHex("fc671a");
+        public static Color QTGray = Color.FromHex("a9a9a9");
     }
 }
