218aa9b451601f31d27343a524f291853d951bca
Rimaz <rimazmohommed523@gmail.com>
2020-03-02 10:24:40 +1300

    update images

--- XamarinDemo/XamarinDemo/ViewModels/FeedViewModel.cs
+++ XamarinDemo/XamarinDemo/ViewModels/FeedViewModel.cs
@@ -17,13 +17,13 @@ namespace XamarinDemo.ViewModels
                 {
                     Heading = "New Testing Show Podcast - Identifying Testing Talent and Being the Testing Talent",
                     Description = "Is you organization truly Agile, or is it stuck in the state of Scrummerfall-- going through bla bla bla",
-                    ImageUrl = "graph_2.PNG"
+                    ImageUrl = "image_1.jpg"
                 },
                 new Feed()
                 {
                     Heading = "Converting to SAP S/4HANA? Dont Neglect These 8 Crucial Questions",
                     Description = "When facing what must be one of the biggest transformational endeavors your organization will bla bla bla",
-                    ImageUrl = "graph_3.png"
+                    ImageUrl = "image_2.jpg"
                 }
             };
 
